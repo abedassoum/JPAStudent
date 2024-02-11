864b14a4783f29c89008dc46077721edc18d186c
Abed Rass <75977129+abedassoum@users.noreply.github.com>
2024-02-11 21:26:54 +0100

    Initial commit

---
+++ src/main/java/org/example/jpastudent/JpaStudentApplication.java
@@ -0,0 +1,14 @@
+package org.example.jpastudent;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaStudentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaStudentApplication.class, args);
+    }
+
+}
